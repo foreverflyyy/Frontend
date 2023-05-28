3de4c3c0d884dcc2d93557c18cc4d55c839a6aa4
Nikita Ushakov <nikita.ushackow.02@gmail.com>
2023-05-28 15:53:14 +0200

    dev: add my own hook usePosts

--- react/react-start/src/App.js
+++ react/react-start/src/App.js
@@ -1,37 +1,19 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
 import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/modal/MyModal';
 import MyButton from './components/UI/button/MyButton';
+import { usePosts } from './hooks/usePosts';
 
 export default function App() {
 
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'javascript', description: 'Hello' },
-    { id: 2, title: 'c#', description: 'good' },
-    { id: 3, title: 'python', description: 'fine' },
-  ]);
-
+  const [posts, setPosts] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
-
-  const [filter, setFilter] = useState({
-    query: '',
-    sort: '',
-  });
-
-  const sortedPosts = useMemo(() => {
-    if (filter.sort)
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()));
-  }, [filter.query, sortedPosts]);
-
+  const [filter, setFilter] = useState({query: '', sort: ''});
+  const sortedAndSearchedPosts = usePosts(posts, filter.query, filter.sort);
+  
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModalVisible(false);
@@ -43,13 +25,13 @@ export default function App() {
 
   return (
     <div className="App">
-      <MyButton style={{marginTop: '30px'}} onClick={() => setModalVisible(true)}>
+      <MyButton style={{ marginTop: '30px' }} onClick={() => setModalVisible(true)}>
         Create post
       </MyButton>
       <MyModal visible={modalVisible} setVisible={(value) => setModalVisible(value)}>
         <PostForm create={createPost} />
       </MyModal>
-      <PostFilter filter={filter} setFilter={setFilter}/>
+      <PostFilter filter={filter} setFilter={setFilter} />
       <PostList remove={removePost} posts={sortedAndSearchedPosts} title='List posts:' />
     </div>
   );
