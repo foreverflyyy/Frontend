a4e09ea8492e4c8ca81c8c1568c642b465ee0d49
Nikita Ushakov <nikita.ushackow.02@gmail.com>
2023-05-28 15:39:00 +0200

    dev: add animation exactly react-transition group

--- react/react-start/src/components/PostList.jsx
+++ react/react-start/src/components/PostList.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import PostItem from './PostItem';
+import PostItem from './PostItem'
+import '../styles/App.css'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 export default function PostList({remove, posts, title}) {
 
@@ -16,9 +18,17 @@ export default function PostList({remove, posts, title}) {
         <h1 style={{textAlign: 'center'}}>
             {title}
         </h1>
-        {posts.map((post, index) => 
-            <PostItem key={post.Id} remove={remove} number={index + 1} post={post}/>
-        )};
+        <TransitionGroup>
+            {posts.map((post, index) => 
+                <CSSTransition
+                    key={post.Id}
+                    timeout={500}
+                    classNames="post"
+                >
+                    <PostItem remove={remove} number={index + 1} post={post}/>
+                </CSSTransition>
+            )}
+        </TransitionGroup>
     </div>
   )
 }
