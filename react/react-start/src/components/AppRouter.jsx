569d7fee504ddf302cf263aa5893d7f4e0ca7d32
Nikita Ushakov <nikita.ushackow.02@gmail.com>
2023-05-30 21:40:30 +0200

    add: finished course on React

--- react/react-start/src/components/AppRouter.jsx
+++ react/react-start/src/components/AppRouter.jsx
@@ -1,29 +1,31 @@
-import React, { useContext } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import About from '../pages/About'
-import Posts from '../pages/Posts'
-import Error from '../pages/Error'
-import { PostIdPage } from '../pages/PostIdPage'
-import Login from '../pages/Login'
-import { AuthContext } from '../context'
+import React, { useContext } from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "../pages/About";
+import Posts from "../pages/Posts";
+import Error from "../pages/Error";
+import { PostIdPage } from "../pages/PostIdPage";
+import Login from "../pages/Login";
+import { AuthContext } from "../context";
+import MyLoader from "./UI/Loader/MyLoader";
 
 export default function AppRouter() {
+  const { isAuth, isLoading } = useContext(AuthContext);
 
-    const {isAuth} = useContext(AuthContext);
+  if(isLoading){
+    return <MyLoader/>
+  }
 
-  return (
-    isAuth
-        ? 
-        <Routes>
-            <Route path='/about' element={<About/>}/>
-            <Route path='/posts/:id' element={<PostIdPage/>}/>
-            <Route path='/posts' element={<Posts/>}/>
-            <Route path="/*" element={<Error/>}/>
-        </Routes>
-        :
-        <Routes>
-            <Route path='/login' element={<Login/>}/>
-            <Route path="/*" element={<Login/>}/>
-        </Routes>
-  )
+  return isAuth ? (
+    <Routes>
+      <Route path="/about" element={<About />} />
+      <Route path="/posts/:id" element={<PostIdPage />} />
+      <Route path="/posts" element={<Posts />} />
+      <Route path="/*" element={<Error />} />
+    </Routes>
+  ) : (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/*" element={<Login />} />
+    </Routes>
+  );
 }
