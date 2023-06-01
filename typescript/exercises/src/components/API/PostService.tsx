b5ecca626b8a02b68975f2df9dd600fedc0be534
Nikita Ushakov <nikita.ushackow.02@gmail.com>
2023-06-01 16:58:33 +0200

    dev: training react + typescript

---
+++ typescript/exercises/src/components/API/PostService.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PostService() {
+  return (
+    <div>PostService</div>
+  )
+}
---
+++ typescript/exercises/src/components/AppRouter.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Posts from '../pages/Posts';
+import Todo from '../pages/Todo';
+
+const AppRouter = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<Home/>}>
+            <Route path='posts' element={<Posts/>}/>
+            <Route path='todo' element={<Todo/>}/>
+        </Route>
+    </Routes>
+  )
+}
+
+export default AppRouter;
